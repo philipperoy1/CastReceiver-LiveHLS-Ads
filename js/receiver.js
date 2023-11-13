20816d4731e5dfc681f5204507da643ea40ed1b2
Philippe Roy <phil@unldeuxp.com>
2023-11-13 09:48:47 -0500

    Live HLS + Ads

--- js/receiver.js
+++ js/receiver.js
@@ -1,189 +1,41 @@
 const context = cast.framework.CastReceiverContext.getInstance();
-const options = new cast.framework.CastReceiverOptions();
 
 const playerManager = context.getPlayerManager();
 
-const mediaData = {};
-
-//Media Sample API Values
-// const SAMPLE_URL =
-//   "https://storage.googleapis.com/cpe-sample-media/content.json";
-const SAMPLE_URL = "./content.json";
-const StreamType = {
-  DASH: "application/dash+xml",
-  HLS: "application/x-mpegurl",
+const mediaData = {
+  contentType: "application/x-mpegurl",
+  streamType: "LIVE",
+  contentUrl:
+    "https://cph-p2p-msl.akamaized.net/hls/live/2000341/test/master.m3u8",
+  adTagUrl:
+    "https://pubads.g.doubleclick.net/gampad/ads?iu=/21775744923/external/vmap_ad_samples&sz=640x480&cust_params=sample_ar%3Dpreonly&ciu_szs=300x250%2C728x90&gdfp_req=1&ad_rule=1&output=vmap&unviewed_position_start=1&env=vp&impl=s&correlator=" +
+    Math.floor(Math.random() * 10000),
 };
-const TEST_STREAM_TYPE = StreamType.HLS;
-const TEST_CONTENT_ID = "test";
-
-// Debug Logger
-const castDebugLogger = cast.debug.CastDebugLogger.getInstance();
-const LOG_TAG = "MyAPP.LOG";
-
-// Enable debug logger and show a 'DEBUG MODE' overlay at top left corner.
-castDebugLogger.setEnabled(true);
-
-// Show debug overlay
-// castDebugLogger.showDebugLogs(true);
-
-// Set verbosity level for Core events.
-castDebugLogger.loggerLevelByEvents = {
-  "cast.framework.events.category.CORE": cast.framework.LoggerLevel.INFO,
-  "cast.framework.events.EventType.MEDIA_STATUS":
-    cast.framework.LoggerLevel.DEBUG,
-};
-
-// Set verbosity level for custom tags.
-castDebugLogger.loggerLevelByTags = {
-  LOG_TAG: cast.framework.LoggerLevel.DEBUG,
-};
-
-function makeRequest(method, url) {
-  console.log("!!!!!!!!! makeRequest url", url);
-
-  return new Promise(function (resolve, reject) {
-    let xhr = new XMLHttpRequest();
-    xhr.open(method, url);
-    xhr.onload = function () {
-      if (this.status >= 200 && this.status < 300) {
-        resolve(JSON.parse(xhr.response));
-      } else {
-        reject({
-          status: this.status,
-          statusText: xhr.statusText,
-        });
-      }
-    };
-    xhr.onerror = function () {
-      reject({
-        status: this.status,
-        statusText: xhr.statusText,
-      });
-    };
-    xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
-    xhr.send();
-  });
-}
 
 playerManager.setMessageInterceptor(
   cast.framework.messages.MessageType.LOAD,
   (request) => {
-    castDebugLogger.info(LOG_TAG, "Intercepting LOAD request");
-
-    // Map contentId to entity
-    // if (request.media && request.media.entity) {
-    request.media.contentId = TEST_CONTENT_ID;
-    // }
-
-    console.log("request.media.contentId !!!!!!!!!", request.media.contentId);
-
     return new Promise((resolve, reject) => {
-      // Fetch repository metadata
-      makeRequest("GET", SAMPLE_URL).then(function (data) {
-        // Obtain resources by contentId from downloaded repository metadata.
-        let item = data[request.media.contentId];
-        if (!item) {
-          // Content could not be found in repository
-          castDebugLogger.error(LOG_TAG, "Content not found");
-          reject();
-        } else {
-          // Adjusting request to make requested content playable
-          request.media.contentType = TEST_STREAM_TYPE;
+      // Stream
+      request.media.contentUrl = mediaData.contentUrl;
+      request.media.contentType = mediaData.contentType;
+      request.media.streamType = mediaData.streamType;
 
-          // Configure player to parse DASH content
-          if (TEST_STREAM_TYPE == StreamType.DASH) {
-            request.media.contentUrl = item.stream.dash;
-          }
+      // ADS
+      let vastTemplate = new cast.framework.messages.VastAdsRequest();
+      vastTemplate.adTagUrl = mediaData.adTagUrl;
+      request.media.vmapAdsRequest = vastTemplate;
 
-          // Configure player to parse HLS content
-          else if (TEST_STREAM_TYPE == StreamType.HLS) {
-            request.media.contentUrl = item.stream.hls;
-            request.media.hlsSegmentFormat =
-              cast.framework.messages.HlsSegmentFormat.FMP4;
+      // METADATA
+      let metadata = new cast.framework.messages.GenericMediaMetadata();
+      metadata.title = "Title";
+      metadata.subtitle = "Author";
+      request.media.metadata = metadata;
 
-            // const cacheBuster = Math.round(Math.random() * 100000);
-            let vastTemplate = new cast.framework.messages.VastAdsRequest();
-            // vastTemplate.adTagUrl = `https://pubads.g.doubleclick.net/gampad/ads?iu=/21775744923/external/vmap_ad_samples&sz=640x480&cust_params=sample_ar%3Dpremidpostoptimizedpod&ciu_szs=300x250&gdfp_req=1&ad_rule=1&output=vmap&unviewed_position_start=1&env=vp&impl=s&cmsid=496&vid=short_onecue&correlator=${cacheBuster}`;
-            vastTemplate.adTagUrl =
-              "https://pubads.g.doubleclick.net/gampad/ads?iu=/21775744923/external/vmap_ad_samples&sz=640x480&cust_params=sample_ar%3Dpreonly&ciu_szs=300x250%2C728x90&gdfp_req=1&ad_rule=1&output=vmap&unviewed_position_start=1&env=vp&impl=s&correlator=" +
-              Math.floor(Math.random() * 10000);
-
-            request.media.vmapAdsRequest = vastTemplate;
-            request.media.contentType = "application/x-mpegurl";
-
-            // request.media.hlsSegmentFormat =
-            //   cast.framework.messages.HlsSegmentFormat.FMP4;
-            request.media.hlsVideoSegmentFormat =
-              cast.framework.messages.HlsVideoSegmentFormat.FMP4;
-          }
-
-          console.log("!!!!!!!!! request", request);
-
-          castDebugLogger.warn(
-            LOG_TAG,
-            "Playable URL:",
-            request.media.contentUrl
-          );
-
-          // Add metadata
-          let metadata = new cast.framework.messages.GenericMediaMetadata();
-          metadata.title = item.title;
-          metadata.subtitle = item.author;
-
-          request.media.metadata = metadata;
-
-          // Resolve request
-          resolve(request);
-        }
-      });
+      console.log("request = ", request);
+      resolve(request);
     });
   }
 );
 
-// Optimizing for smart displays
-const touchControls = cast.framework.ui.Controls.getInstance();
-const playerData = new cast.framework.ui.PlayerData();
-const playerDataBinder = new cast.framework.ui.PlayerDataBinder(playerData);
-
-let browseItems = getBrowseItems();
-
-function getBrowseItems() {
-  let browseItems = [];
-  makeRequest("GET", SAMPLE_URL).then(function (data) {
-    for (let key in data) {
-      let item = new cast.framework.ui.BrowseItem();
-      item.entity = key;
-      item.title = data[key].title;
-      item.subtitle = data[key].description;
-      item.image = new cast.framework.messages.Image(data[key].poster);
-      item.imageType = cast.framework.ui.BrowseImageType.MOVIE;
-      browseItems.push(item);
-    }
-  });
-  return browseItems;
-}
-
-let browseContent = new cast.framework.ui.BrowseContent();
-browseContent.title = "Up Next";
-browseContent.items = browseItems;
-browseContent.targetAspectRatio =
-  cast.framework.ui.BrowseImageAspectRatio.LANDSCAPE_16_TO_9;
-
-playerDataBinder.addEventListener(
-  cast.framework.ui.PlayerDataEventType.MEDIA_CHANGED,
-  (e) => {
-    if (!e.value) return;
-
-    // Media browse
-    touchControls.setBrowseContent(browseContent);
-
-    // Clear default buttons and re-assign
-    touchControls.clearDefaultSlotAssignments();
-    touchControls.assignButton(
-      cast.framework.ui.ControlsSlot.SLOT_PRIMARY_1,
-      cast.framework.ui.ControlsButton.SEEK_BACKWARD_30
-    );
-  }
-);
-
-context.start(options);
+context.start();
